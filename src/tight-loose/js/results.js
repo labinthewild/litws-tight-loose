09b3e2bfe1b027d0f77d42435740b5e28f29ece7
Nigini Oliveira <nigini@gmail.com>
2024-03-07 11:56:18 -0800

    Finished the baseline for presentin scores in the cultural dimension graphic.

--- src/tight-loose/js/results.js
+++ src/tight-loose/js/results.js
@@ -3,15 +3,19 @@
     const width = 640;
     const height = 400;
     const barHeight = height/10;
+    const scoreMax = 13;
     const marginTop = 20;
     const marginRight = 20;
     const marginBottom = 30;
     const marginLeft = 40;
 
+    let _calculateMarkX = function (score) {
+        return (width/scoreMax*score)
+    }
     let _addMark = function (context){
-        context.moveTo(5,0)
-        context.lineTo(10,10)
-        context.lineTo(0,10)
+        context.moveTo(barHeight/2,barHeight)
+        context.lineTo(0,0)
+        context.lineTo(barHeight, 0)
         context.closePath()
         return context
     }
@@ -23,19 +27,37 @@
             .attr("height", height);
 
         // Add bar
-        svg.append("rect")
+        let bar = svg.append("g");
+        bar.append("rect")
             .attr('x', 0)
             .attr('y', 0)
             .attr('width', width)
             .attr('height', barHeight)
             .attr('stroke', 'black')
             .attr('fill', '#6A9341')
-            .attr('transform', `translate(${0}, ${height/2-(barHeight/2)})`);
+        bar.append("text")
+            .attr('x', 0)
+            .attr('y', barHeight*2)
+            .text($.i18n('study-tl-results-legend-loose'))
+        bar.append("text")
+            .attr('x', width)
+            .attr('y', barHeight*2)
+            .attr('text-anchor', 'end')
+            .text($.i18n('study-tl-results-legend-tight'))
+        bar.attr('transform', `translate(${0}, ${height/2-(barHeight/2)})`);
 
-        svg.append("path")
+        let mark = svg.append("g");
+        mark.append("path")
             .style("stroke", "black")
             .style("fill", "none")
             .attr('d', _addMark(d3.path()))
+        mark.append("text")
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('text-anchor', 'start')
+            .attr('font-size', '1.5em')
+            .text($.i18n('study-tl-results-legend-usa'))
+        mark.attr('transform', `translate(${_calculateMarkX(5.1)}, ${height/2-(3/2*barHeight)})`)
     }
 
     exports.results = {};
